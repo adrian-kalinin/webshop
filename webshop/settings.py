a1a763c728e03e003be70863a5ab6fa4e655dc82
Adrian Kalinin <adriankalinin@protonmail.com>
2021-11-24 04:08:38 +0200

    Fix path in settings.

--- webshop/settings.py
+++ webshop/settings.py
@@ -100,7 +100,7 @@ LOGIN_URL = 'login/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
-STATICFILES_DIRS = str(BASE_DIR / 'static')
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
