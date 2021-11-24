6ba986b38c229def304409a732386619435bf573
Adrian Kalinin <adriankalinin@protonmail.com>
2021-11-25 00:54:41 +0200

    Update gunicorn settings.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,15 +1,8 @@
-from pathlib import Path
-
-
-command = '/usr/bin/gunicorn'
-pythonpath = str(Path(__file__).resolve())
 bind = '0.0.0.0:8080'
 
 workers = 3
 worker_connections = 1000
 timeout = 1200
-limit_request_fields = 32000
-limit_request_field_size = 0
 
 worker_class = 'gevent'
 raw_env = 'DJANGO_SETTINGS_MODULE=webshop.settings'
