2224c33f150dee1655986de39f50a7cf281aada9
Adrian Kalinin <adriankalinin@protonmail.com>
2021-11-25 00:36:05 +0200

    Update gunicorn config.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,12 @@
-bind = '127.0.0.1:8080'
-workers = 2
+command = '/usr/bin/gunicorn'
+pythonpath = ''
+bind = '0.0.0.0:8080'
+
+workers = 3
 worker_connections = 1000
 timeout = 1200
+limit_request_fields = 32000
+limit_request_field_size = 0
+
 worker_class = 'gevent'
 raw_env = 'DJANGO_SETTINGS_MODULE=webshop.settings'
