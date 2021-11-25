abbc52aa7be923cd314d024ef4038ecec5f1bdb0
Adrian Kalinin <adriankalinin@protonmail.com>
2021-11-25 15:13:12 +0200

    Use DigitalOcean Spaces for static and media.

--- webshop/custom_storages.py
+++ webshop/custom_storages.py
@@ -1,13 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-import settings
-
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     location = 'static'
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     location = 'media'
